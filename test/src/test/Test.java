33c38d937570d9417fe1fb321c43b7b21c61f735
clark.yan <clark.yan@wolaidai.com>
2016-08-11 15:41:48 +0800

    345

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 public static void main(String[] args)
 {
 	System.out.println("hello wo123rld");
-	System.out.println("hell456734 85o");
+	System.out.println("hell45685o");
 }
 }
   
