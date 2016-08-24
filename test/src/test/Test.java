4a762711e5f99b288cace19223c4f507f535ba18
clark.yan <clark.yan@wolaidai.com>
2016-08-24 15:35:36 +0800

    sfdsf

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Test {
 public static void main(String[] args)
 {
-	String str=null;山东高速
+	String str=null;山东高速2342
 	System.out.println("hello wo123rld");
 	System.out.println("hell45685o");
 	System.out.println(str.toCharArray());
