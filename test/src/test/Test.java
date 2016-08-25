6d86c29ae819c307fb6939d106061d0bfaeddeb7
hasee <hasee@administrator>
2016-08-25 19:46:28 +0800

    已提交

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
 public static void main(String[] args)
 {
 	String str=null;
-	System.out.println("hello wo12");
+	System.out.println("hello wo124353");
 	System.out.println("hell45685o");
 	System.out.println(str.toCharArray());
 }
