749030b84c70226a77ce311030807b6c1f3ba982
hasee <hasee@administrator>
2016-08-25 20:57:10 +0800

    565

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
 public static void main(String[] args)
 {
 	String str=null;
-	System.out.println("hello wo124454353");
+	System.out.println("hello32 wo124454353");
 	System.out.println("hell45685o");
 	System.out.println(str.toCharArray());
 }
