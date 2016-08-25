da4881ea73e4e9bc02e0fd63c9210b1d7e54eafe
clark.yan <clark.yan@wolaidai.com>
2016-08-25 18:19:06 +0800

    ces

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
 public static void main(String[] args)
 {
 	String str=null;
-	System.out.println("hello wo123rld5445");
+	System.out.println("hello wo123rld5445456");
 	System.out.println("hell45685o");
 	System.out.println(str.toCharArray());
 }
