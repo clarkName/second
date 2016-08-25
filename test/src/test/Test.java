5f55020e08064b0f0796da8fbf530d9ed19602f4
clark.yan <clark.yan@wolaidai.com>
2016-08-25 18:35:45 +0800

    Revert "Revert "再次提交""
    This reverts commit 82e25f40915364e30dd4ee5543e37ce30233847b.

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Test {
 public static void main(String[] args)
 {
-	String str=null;山东高速2342
+	String str=null;山东高速2342342的风格34
 	System.out.println("hello wo123rld");
 	System.out.println("hell45685o");
 	System.out.println(str.toCharArray());
--- test/src/test/Test1.java
+++ test/src/test/Test1.java
@@ -2,5 +2,5 @@ package test;
 
 public class Test1
 {
-568585
+568585sgs3445
 }
