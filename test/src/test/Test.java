7be29aba7568f1ed9617537f74fe450c1f2c9a96
clark.yan <clark.yan@wolaidai.com>
2016-08-29 14:06:55 +0800

    3

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class Test {
 public static void main(String[] args)
 {
-	String str=null; 
-	System.out.println("hello wo123rld");
+	String str=null;
+    System.out.println("hello wo123rld");
 	System.out.println("hell45685o");
 	System.out.println(str.toCharArray());
 }
